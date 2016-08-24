19aa91b7ad0ab2f20a37e4595d3b2a3fe7f061bb
Akihiro Noguchi <aki@aki-null.net>
2016-08-24 17:39:32 +0900

    Config update
    - Smaller program size
    - Shorter tap period

--- keyboard/hhkb/config.h
+++ keyboard/hhkb/config.h
@@ -41,7 +41,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 /* period of tapping(ms) */
-#define TAPPING_TERM    200
+#define TAPPING_TERM    150
 /* tap count needed for toggling a feature */
 #define TAPPING_TOGGLE  5
 /* Oneshot timeout(ms) */
@@ -56,16 +56,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *  These options are also useful to firmware size reduction.
  */
 /* disable debug print */
-//#define NO_DEBUG
+#define NO_DEBUG
 
 /* disable print */
-//#define NO_PRINT
+#define NO_PRINT
 
 /* disable action features */
 //#define NO_ACTION_LAYER
 //#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
-//#define NO_ACTION_MACRO
+#define NO_ACTION_ONESHOT
+#define NO_ACTION_MACRO
 //#define NO_ACTION_FUNCTION
 
 #endif
